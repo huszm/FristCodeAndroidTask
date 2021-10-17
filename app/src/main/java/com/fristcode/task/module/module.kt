1db92525be272dfa4d53d7560e76b6ee8b455afe
hosam_mac <hosamghw@outlook.com>
2021-10-17 11:49:48 +0300

    make new changes

--- app/src/main/java/com/fristcode/task/module/module.kt
+++ app/src/main/java/com/fristcode/task/module/module.kt
@@ -5,36 +5,33 @@ import com.fristcode.task.api.RetrofitClient
 import com.fristcode.task.database.PostDao
 import com.fristcode.task.database.PostDatabase
 import com.fristcode.task.repository.Repository
-import com.fristcode.task.ui.addPost.AddPostFragment
 import com.fristcode.task.ui.addPost.AddPostViewModel
-import com.fristcode.task.ui.editPost.EditPostFragment
 import com.fristcode.task.ui.editPost.EditPostViewModel
 import com.fristcode.task.ui.postDetails.PostDetailsViewModel
-import com.fristcode.task.ui.postsList.PostsListFragment
 import com.fristcode.task.ui.postsList.PostsListViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val appModule = module (override = true) {
-    single { Repository(get() , get()) }
+val appModule = module(override = true) {
+    single { Repository(get(), get()) }
 }
 
 
-val roomModule = module  (override = true){
+val roomModule = module(override = true) {
     fun provideRoom(): PostDao {
         return PostDatabase.getDatabase().postDao()
     }
     single { provideRoom() }
 }
 
-val retrofitServiceModule = module  (override = true){
+val retrofitServiceModule = module(override = true) {
     fun provideRetrofit(): Api {
         return RetrofitClient.retrofit.create(Api::class.java)
     }
     single { provideRetrofit() }
 }
 
-val viewModelModule = module (override = true){
+val viewModelModule = module(override = true) {
 
     viewModel { PostsListViewModel(get()) }
 
